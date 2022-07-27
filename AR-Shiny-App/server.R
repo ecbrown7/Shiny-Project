94020d4786ff6f1e735978163ff3429071376783
ecbrown7 <ecbrown7@ncsu.edu>
2022-07-27 13:00:42 -0400

    Chemlist/cyplist widget for data exploration page

--- AR-Shiny-App/server.R
+++ AR-Shiny-App/server.R
@@ -1,20 +1,110 @@
 # This is the server logic of a Shiny web application. You can run the
 
-
 library(shiny)
+library(data.table)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
+#Reading in data, modifying data, creating master file called "study" that contains all information in AR2 Antagonist Ref 128 Study
+
+# read in dpid_128 chem plate map#
+dpid.128 = read.csv("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/validation_chem.csv", stringsAsFactors = F, skipNul = T)
+dpid.128 = as.data.table(dpid.128)
+
+# read in control plate map#
+control = read.csv("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/antagonist_control_chem.csv", stringsAsFactors = F, skipNul = T)
+control = as.data.table(control)
+
+# read in cohort dispense maps#
+dispmap1 = read.csv("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/valid_ar_antag_cohort_1.map.csv", stringsAsFactors = F, skipNul = T)
+dispmap1$cohort = 1
+dispmap2 = read.csv("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/valid_ar_antag_cohort_2.map.csv", stringsAsFactors = F, skipNul = T)
+dispmap2$cohort = 2
+dispmap3 = read.csv("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/valid_ar_antag_cohort_3.map.csv", stringsAsFactors = F, skipNul = T)
+dispmap3$cohort = 3
+
+dispmap = rbind(dispmap1, dispmap2, dispmap3)
+dispmap = data.table(Source.Plate = dispmap$Source.Plate.Barcode, Source.Well = dispmap$Source.Well, Destination.Well = dispmap$Destination.Well, 
+                     Transfer.Volume = dispmap$Transfer.Volume,
+                     cpid = dispmap$apid, cohort = dispmap$cohort)
+
+# merge in chnm and stock.mm from source plate maps#
+dispmap$chnm = ifelse(dispmap$Source.Plate == "DPID_128", dpid.128$chnm[match(dispmap$Source.Well, dpid.128$well)], 
+                      ifelse(dispmap$Source.Plate == "CNTRL", control$chnm[match(dispmap$Source.Well, control$well)], 
+                             ifelse(dispmap$Source.Well == "D1", "DMSO", NA)))
+dispmap$stock.mm = ifelse(dispmap$Source.Plate == "DPID_128", dpid.128$stock.mM[match(dispmap$Source.Well, dpid.128$well)], 
+                          ifelse(dispmap$Source.Plate == "CNTRL", control$stock.mM[match(dispmap$Source.Well, control$well)], NA))
+
+# read in filemap #
+filemap = read.csv("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/AR_Antagonist128_Met_filemap.csv", stringsAsFactors = F, skipNul = T)
+filemap = data.table(filemap)
+
+### map data data ###
+filemap = filemap[!TR %in% 2874:2913,]
+
+study = NULL
+for (j in 1:nrow(filemap)){
+    
+    file.name = paste("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/data/TRno", filemap$TR[j], ".CSV", sep = "")
+    sub = read.csv(file.name, stringsAsFactors = F, skipNul = T)
+    sub$X = NULL
+    names(sub) = c("rowi", "coli", "sample", "RLU")
+    sub$apid = filemap$TR[j]
+    sub$well = paste(sub$rowi,sub$coli,sep="")
+    sub$replicate = filemap$replicate[j]
+    sub$biogroup = filemap$biogroup[j]
+    sub$assay = filemap$assay[j]
+    
+    dat = data.table(Destination.Well = sub$well, RLU = sub$RLU, biogroup = sub$biogroup, replicate = sub$replicate, apid = sub$apid,
+                     assay = sub$assay)
+    merged = merge.data.frame(dispmap[cohort == filemap$cohort[j] & cpid == filemap$cpid[j],], dat, by = "Destination.Well")
+    
+    study = rbind(study,merged)
+    
+}
+study = as.data.table(study)
+
+study[, final.um := stock.mm * Transfer.Volume / (45 + (Transfer.Volume + 7.5)/1000)]
+study[, logc := log10(final.um)]
+
+bval.table = study[, median(RLU), by = .(apid, chnm)][chnm == "DMSO",]
+study$bval = bval.table$V1[match(study$apid, bval.table$apid)]
+
+pval1.table = study[, median(RLU), by = .(apid, chnm, logc)][chnm == "BICAL" & logc > 1.9,]
+study$pval1 = pval1.table$V1[match(study$apid, bval.table$apid)]
+
+pval2.table = study[, median(RLU), by = .(apid, chnm, logc)][chnm == "DCLN" & logc > 1.9,]
+study$pval2 = pval2.table$V1[match(study$apid, bval.table$apid)]
+
+study[, nval := ifelse(assay == "AR", (RLU - bval)/(pval1-bval)*100, (RLU - bval)/(pval2-bval)*100)]
+setkey(study, chnm)
+cmpd1 = study[!is.na(logc),]
+#Finalized data set
+AR2study_complete = cmpd1[assay == "AR",]
+
+
+
+#plot dose-response AR data#
+#Source code written to plot hill curves through EPA offline pipeline
+#source("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/tcplFit.R")
+#source("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/tcpl_Fit_Lite_Sample_Data.R")
+#source("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/tcplObjCnst.R")
+#source("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/tcplObjHill.R")
+#source("C:/Users/Ebrown08/OneDrive - Environmental Protection Agency (EPA)/Profile/Desktop/ST558/Shiny-Project/ar_shiny_data/tcplObjGnls.R")
+
+#hill_curve = function(hill_tp, hill_ga, hill_gw, lconc){
+#    return(hill_tp/(1+10^((hill_ga - lconc)*hill_gw)))
+#}
+
+
+#Shiny Server
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    output$filteredData <- renderDataTable({
+         filtered <- AR2study_complete %>% filter(chnm == input$chemList) %>% filter(biogroup == input$cypList)
+         filtered2 <- filtered %>% select(2,7,13,10,5:9,11:12,14:19)
+         filtered2
     })
 
 })
+
+
--- AR-Shiny-App/ui.R
+++ AR-Shiny-App/ui.R
@@ -13,12 +13,42 @@ shinyUI(navbarPage("AR2 Assay Data App", theme = shinytheme("flatly"),
                     sidebarPanel("Description"),
                     mainPanel())),
                    
-                   tabPanel("Data Visualization",
-                            titlePanel("AR2 Antagonist Plots"),
+                   tabPanel("Data Exploration",
+                            titlePanel("Exploring Study Data"),
                      sidebarLayout(
-                     sidebarPanel("Description"),
-                     mainPanel())),
+                     sidebarPanel(
+                     selectInput(
+                       "chemList",
+                       "Select Chemical: ",
+                       choices = c("17-Methyltestosterone", "17alpha-Estradiol", "17alpha-Ethinylestradiol","17beta-Estradiol", "17beta-Trenbolone",
+                       "2,2',4,4'-Tetrahydroxybenzophenone","2,4-Dihydroxybenzophenone","2,4-Dinitrophenol","2-Ethylhexylparaben","4,4'-Sulfonyldiphenol",
+                       "4-(1,1,3,3-Tetramethylbutyl)phenol","4-(2-Methylbutan-2-yl)phenol","4-Androstene-3,17-dione","4-Cumylphenol","4-Dodecylphenol",
+                       "4-Hydroxybenzoic acid","4-Nonylphenol","4-tert-Butylphenol","5alpha-Dihydrotestosterone","Abamectin","Acephate","Afimoxifene (4-Hydroxytamoxifen)",
+                       "Amitrole","Anastrozole","Apigenin","Atrazine","BICAL","Benfluralin","Benomyl","Bicalutamide","Bifenthrin","Bis(2-ethylhexyl)hexanedioate",
+                       "Bis(2-ethylhexyl)phthalate","Bisphenol A","Bisphenol AF","Bisphenol B","Butylbenzylphthalate","Butylparaben","Carbendazim","Carbofuran",
+                       "Chlorothalonil","Chlorpyrifos","Chlorpyrifos-methyl","Clomiphene citrate","Corticosterone","Coumestrol ","Cyfluthrin","Cypermethrin",
+                       "Cyproterone acetate","DCLN","Daidzein ","Danazol","Deltamethrin","Diazinon","Dibutyl phthalate","Dichlobenil",
+                       "Dichlorodiphenyltrichloroethane","Dicyclohexyl phthalate","Diethyl phthalate","Diethylstilbestrol","Dihexyl phthalate","Dipentyl phthalate",
+                       "Enclomiphene hydrochloride","Equilin","Esfenvalerate","Estrone","Ethoprop","Ethylparaben","Exemestane","Fenarimol","Fenitrothion","Fenthion",
+                       "Fenvalerate","Finasteride","Flutamide","Flutolanil","Folpet","Formestane","Fulvestrant","Genistein","Hydroxyflutamide",
+                       "Hydroxyprogesterone caproate","Iprodione","Kaempferol","Letrozole","Levonorgestrel","Linuron","Malathion","Mestranol","Metalaxyl","Methomyl",
+                       "Methoxychlor","Metolachlor","Metribuzin","Mifepristone","Mono(2-ethylhexyl) phthalate","Naringenin","Nilutamide","Norethindrone","Norflurazon",
+                       "Octamethylcyclotetrasiloxane","Octylbicycloheptenedicarboximide","Oxamyl","Pentachloronitrobenzene","Pentachlorophenol","Permethrin",
+                       "Phenothrin","Phosmet","Prochloraz","Procymidone","Propargite","Propiconazole","Propyzamide","Raloxifene hydrochloride","Simazine",
+                       "Spironolactone","Tamoxifen","Tebuconazole","Testosterone propionate","Tetramethrin","Triadimefon","Tributylchlorostannane",
+                       "Trifluralin","Vinclozolin","Z-Tetrachlorvinphos" ,"Zearalenone","o,p'-DDT","p,p'-DDD","p,p'-DDE","p-Dichlorobenzene")),
+                     selectInput(
+                       "cypList",
+                       "Select Biogroup: ",
+                       choices = c("noRNA","Bgal","CYP1A2","CYP2A6","CYP2B6","CYP2C8","CYP2C9","CYP2C19","CYP2D6","CYP2E1","CYP2E1-WT","CYP2J2","CYP3A4"))),
+                     mainPanel(dataTableOutput("filteredData")))),
         
+                   tabPanel("Data Visualization",
+                            titlePanel("AR Antagonist Plots"),
+                            sidebarLayout(
+                              sidebarPanel("Description"),
+                              mainPanel())),
+                   
                    tabPanel("Modeling",
                             titlePanel("Chemotype Modeling for AR2 Assay Hits"),
                       sidebarLayout(
